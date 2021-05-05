5a53daf4f3b3f9f316a50799deebde799ca975dc
AadiRealm111 <54953936+AadiRealm111@users.noreply.github.com>
2021-05-06 02:42:42 +0530

    Add files via upload

---
+++ lib/screens/models/request.dart
@@ -0,0 +1,9 @@
+class Requests {
+  final String name;
+  final String email;
+  final String address;
+  final String request;
+  final String phone;
+
+  Requests({this.name, this.email, this.address, this.request, this.phone});
+}

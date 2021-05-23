0430b29a2c8b3b258e119205b15a844ccb4e060c
Akshay Jain <1880akshay@gmail.com>
2021-05-23 13:59:36 +0530

    try catch block added

--- index.js
+++ index.js
@@ -4,32 +4,41 @@ const { admin } = require('./firebase-config');
 const app = express();
 app.use(express.json());
 
-const port = 3000;
+const port = process.env.PORT || 5000;
 
 const db = admin.firestore();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
 
 app.post('/firebase/notification', async (req, res) => {
     var sender = await db.collection('users').doc(req.body.uid1).get();
     db.collection('users').doc(req.body.uid2).get().then(receiver => {
-        admin.messaging().sendToDevice(
-            receiver.data()['tokens'],
-            {
-                data: {
-                    title: 'New Message',
-                    body: 'You have a new message from '+sender.data()['name'],
-                    payload: JSON.stringify({
-                        uid1: req.body.uid2,
-                        uid2: req.body.uid1,
-                    })
-                },
-            }
-        ).then( response => {
-            console.log("Notification sent successfully");
-            res.status(200).send("Notification sent successfully");
-        })
-        .catch( error => {
-            console.log(error);
-        });
+        try {
+            admin.messaging().sendToDevice(
+                receiver.data()['tokens'],
+                {
+                    data: {
+                        title: 'New Message',
+                        body: 'You have a new message from '+sender.data()['name'],
+                        payload: JSON.stringify({
+                            uid1: req.body.uid2,
+                            uid2: req.body.uid1,
+                        })
+                    },
+                }
+            ).then( response => {
+                console.log("Notification sent successfully");
+                res.status(200).send("Notification sent successfully");
+            })
+            .catch( error => {
+                console.log(error);
+            });
+        } catch(e) {
+            console.log("No active device found");
+            res.status(200).send("No active device found");
+        }  
     }); 
 })
 
